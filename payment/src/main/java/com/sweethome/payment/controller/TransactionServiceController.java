990c3d850a0637fe9310acf492dc79fb6814c5a8
Arjun-47 <nagarjun.chittyala447@gmail.com>
2023-04-26 20:16:25 +0530

    Added validation to createTransaction call Updated application.properties in payment service to point to payments database API Gateway is introduced to the project

--- payment/src/main/java/com/sweethome/payment/controller/TransactionServiceController.java
+++ payment/src/main/java/com/sweethome/payment/controller/TransactionServiceController.java
@@ -1,13 +1,17 @@
 package com.sweethome.payment.controller;
 
+import com.sweethome.payment.constants.PaymentMode;
 import com.sweethome.payment.dto.TransactionDTO;
 import com.sweethome.payment.model.TransactionDetailsEntity;
 import com.sweethome.payment.service.PaymentService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/payment")
@@ -21,13 +25,29 @@ public class TransactionServiceController {
 
     @RequestMapping(value = "/transaction", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Integer> createTransaction(@RequestBody TransactionDTO transactionDTO) {
-        TransactionDetailsEntity transactionDetailsEntity = modelMapper.map(transactionDTO, TransactionDetailsEntity.class);
-        return ResponseEntity.status(201).body(paymentService.createTransaction(transactionDetailsEntity));
-    }
 
+        boolean isValidRequest;
+        if(transactionDTO.getPaymentMode().equalsIgnoreCase(PaymentMode.UPI)){
+            isValidRequest = !(Objects.isNull(transactionDTO.getUpiId()) || transactionDTO.getUpiId().isEmpty());
+            transactionDTO.setCardNumber(null);
+        } else if(transactionDTO.getPaymentMode().equalsIgnoreCase(PaymentMode.CARD)){
+            isValidRequest = !(Objects.isNull(transactionDTO.getCardNumber()) || transactionDTO.getCardNumber().isEmpty());
+            transactionDTO.setUpiId(null);
+        } else{
+            isValidRequest = false;
+        }
+        if(isValidRequest){
+            TransactionDetailsEntity transactionDetailsEntity = modelMapper.map(transactionDTO, TransactionDetailsEntity.class);
+            return new ResponseEntity<>(paymentService.createTransaction(transactionDetailsEntity), HttpStatus.CREATED);
+        } else{
+            return new ResponseEntity<>(0, HttpStatus.BAD_REQUEST);
+        }
+    }
 
     @RequestMapping(value = "/transaction/{transactionId}", method = RequestMethod.GET)
     public ResponseEntity<TransactionDetailsEntity> getTransaction(@PathVariable(name = "transactionId") int transactionId) {
-        return ResponseEntity.ok(paymentService.getTransaction(transactionId));
+        TransactionDetailsEntity transactionDetailsEntity = paymentService.getTransaction(transactionId);
+        return Objects.isNull(transactionDetailsEntity) ? new ResponseEntity<>(null, HttpStatus.NOT_FOUND)
+                : new ResponseEntity<>(transactionDetailsEntity, HttpStatus.OK);
     }
 }
